da5da4425352e0ec4dfc819f40b54ee9e43f5997
Jacob Goldverg <jgoldverg@gmail.com>
2023-08-03 15:18:26 -0500

    crud operations on env works now

--- floto/api/__init__.py
+++
@@ -1,3 +0,0 @@
-import balena
-
-balena.settings.Settings().set("api_endpoint", "https://api.floto.science/")
--- floto/api/urls.py
+++ floto/api/urls.py
@@ -6,4 +6,5 @@ router = DefaultRouter()
 router.register("devices", views.DeviceViewSet, basename="device")
 router.register("fleets", views.FleetViewSet, basename="fleet")
 router.register("collections", views.CollectionViewSet, basename="collection")
+router.register("envs", views.EnvViewSet, basename="env")
 urlpatterns = router.urls
