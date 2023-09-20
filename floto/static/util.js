699c431821f3cfd0dfa1a74b0a7ca8b704b4c53a
Mark Powers <markpowers@uchicago.edu>
2023-09-20 09:01:43 -0500

    Add notification on API fetch error callback.

--- floto/static/util.js
+++ floto/static/util.js
@@ -1,15 +1,16 @@
-function fetch_with_retry(url, callback, error_callback=function(r){}, backoff_count=0) {
+function fetch_with_retry(url, callback, error_callback=base_error_callback, backoff_count=0) {
   fetch(url).then((response) => {
     if (response.ok) {
       return response.json();
     }
     return Promise.reject(response);
   }).then(callback).catch((response) => {
+    console.log("catching error")
     if(response.status < 500){
       // Client error, we can't do anything bout it
-      console.log("unauthorized...")
+      console.log("calling error callback")
+      error_callback(response)
     } else {
-      console.error(response)
       if(backoff_count > 3){
         console.error("failed after 3 backoff attempts")
         error_callback(response)
@@ -44,5 +45,21 @@ function process_created_by(obj){
   obj.is_owned_by_current_user = obj.created_by == get_user_email()
   if ("environment" in obj){
     obj.parsed_env = JSON.parse(obj.environment)
+  }
+}
+
+function base_error_callback(response){
+  console.info("ERROR CALLBACK")
+  if (response.status == 403){
+    Quasar.Notify.create({
+      color: 'negative',
+      message: 'You are not authorized to do that. Please contact an administrator if you believe this to be a mistake.', 
+      icon: 'report_problem',
+      multiLine: true,
+      timeout: 0,
+      actions: [
+        { icon: 'close', color: 'white', round: true, handler: () => {} }
+      ],
+    })
   }
 }
