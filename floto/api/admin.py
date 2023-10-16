4cc524bfd702354bc6494bc80f88551108fff730
Mark Powers <markpowers@uchicago.edu>
2023-10-16 11:46:43 -0500

    Add admin panel timeslots

--- floto/api/admin.py
+++ floto/api/admin.py
@@ -19,15 +19,40 @@ admin.site.register(models.Service, ServiceAdmin)
 class ApplicationServiceInline(admin.StackedInline):
     model = models.ApplicationService
 
+
 class ApplicationAdmin(admin.ModelAdmin):
     list_display = ["name", "created_by", "created_at"]
     inlines = (ApplicationServiceInline,)
 
+
 admin.site.register(models.Application, ApplicationAdmin)
+
+
+class JobTimeslotInline(admin.StackedInline):
+    model = models.JobTiming
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
+class JobDeviceInline(admin.StackedInline):
+    model = models.JobDevice
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
+class DeviceTimeslotInline(admin.StackedInline):
+    model = models.DeviceTimeslot
+
+    def has_add_permission(self, request, obj=None):
+        return False
 
 
 class JobAdmin(admin.ModelAdmin):
     list_display = ["uuid", "created_by", "created_at"]
+    inlines = (
+        JobDeviceInline, JobTimeslotInline, DeviceTimeslotInline,)
 
 
 admin.site.register(models.Job, JobAdmin)
