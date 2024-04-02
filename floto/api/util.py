c16e86fb8c697db40aa57bc24e84c9a32cb1df48
Mark Powers <markpowers@uchicago.edu>
2024-04-02 15:44:01 -0500

    Update overview with job status and map clusters (#21)
    * Add job chart
    
    * Add map cluster icons

--- floto/api/util.py
+++ floto/api/util.py
@@ -148,3 +148,16 @@ def parse_timings(timings, devices, application_uuid):
                             "reason": f"Node port '{port}' is already in use on device",
                         })
     return res
+
+
+def parse_javascript_iso_string(date_str):
+    """
+    Javascript ISO date format adds "Z" to the end, but python
+    expects "+00:00". 
+    """
+    try:
+        return datetime.fromisoformat(date_str.replace("Z", "+00:00"))
+    except Exception as e:
+        LOG.error(f"Exception parsing date '{date_str}'")
+        LOG.error(e)
+        return None
